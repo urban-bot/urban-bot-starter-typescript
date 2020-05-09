c913c44633c5704a1be3a19a5becfbef0dc777e9
ledamint <heresliker@gmail.com>
2020-05-09 22:54:45 +0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import React, { useState } from 'react';
-import { Text, useText } from 'urban-bot';
+import React from 'react';
+import { Route, Router, Text, ButtonGroup, Button, useText } from 'urban-bot';
 
-export function App() {
-    const [text, setText] = useState('Say hi');
+function Echo() {
+    const [text, setText] = React.useState('Say something');
 
     useText(({ text }) => {
         setText(text);
     });
 
-    return <Text>{text}</Text>;
+    return (
+        <Text isNewMessageEveryRender>
+            <i>{text}</i>
+        </Text>
+    );
+}
+
+function Counter() {
+    const [count, setCount] = React.useState(0);
+
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+
+    return (
+        <ButtonGroup title={count}>
+            <Button onClick={increment}>+1</Button>
+            <Button onClick={decrement}>-1</Button>
+        </ButtonGroup>
+    );
+}
+
+export function App() {
+    return (
+        <Router>
+            <Route path="/start">
+                <Echo />
+            </Route>
+            <Route path="/counter">
+                <Counter />
+            </Route>
+        </Router>
+    );
 }
