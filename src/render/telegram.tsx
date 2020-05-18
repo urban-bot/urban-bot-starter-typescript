acd02b9ca9a4aa6702aa6a575f0d6b71c6705a96
ledamint <heresliker@gmail.com>
2020-05-18 20:51:42 +0300

    update telegram

--- src/render/telegram.tsx
+++ src/render/telegram.tsx
@@ -6,17 +6,30 @@ import { App } from '../App';
 
 dotenv.config();
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 if (process.env.TELEGRAM_TOKEN === undefined) {
     throw new Error('Provide TELEGRAM_TOKEN to .env https://core.telegram.org/bots#6-botfather');
 }
 
-const urbanBotTelegram = new UrbanBotTelegram(process.env.TELEGRAM_TOKEN, {
-    polling: true,
-});
+let options;
+if (isDevelopment) {
+    options = {
+        polling: true,
+    };
+} else {
+    options = {
+        webHook: {
+            port: process.env.PORT ? Number(process.env.PORT) : undefined,
+        },
+    };
+}
+
+const urbanBotTelegram = new UrbanBotTelegram(process.env.TELEGRAM_TOKEN, options);
 
 render(
-    <Root bot={urbanBotTelegram} parseMode="HTML">
+    <Root bot={urbanBotTelegram}>
         <App />
     </Root>,
-    () => console.log('telegram bot is started'),
+    () => console.log('telegram bot has started'),
 );
