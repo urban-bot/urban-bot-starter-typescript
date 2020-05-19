5c0e4771e57c42ef87805f17f470a9163a0751fe
ledamint <heresliker@gmail.com>
2020-05-19 21:28:31 +0300

    add exclude test to tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
-        project: './tsconfig.json',
+        project: './tsconfig-eslint.json',
         tsconfigRootDir: '.',
         ecmaFeatures: {
             jsx: true,
