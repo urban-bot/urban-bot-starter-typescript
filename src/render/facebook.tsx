5f7448d0f7b0081099179ff64b475ec427488b2d
ledamint <heresliker@gmail.com>
2020-05-31 18:19:39 +0300

    fix facebook errros

--- src/render/facebook.tsx
+++ src/render/facebook.tsx
@@ -9,11 +9,21 @@ dotenv.config();
 const { FACEBOOK_APP_SECRET, FACEBOOK_PAGE_ACCESS_TOKEN, FACEBOOK_VERIFY_TOKEN, PORT } = process.env;
 
 if (FACEBOOK_APP_SECRET === undefined) {
-    throw new Error('Provide FACEBOOK_APP_SECRET to .env https://developers.facebook.com/apps/');
+    throw new Error(
+        'Provide FACEBOOK_APP_SECRET to .env https://developers.facebook.com/apps/<YOUR_APP_ID>/settings/basic/',
+    );
 }
 
 if (FACEBOOK_PAGE_ACCESS_TOKEN === undefined) {
-    throw new Error('Provide FACEBOOK_PAGE_ACCESS_TOKEN to .env https://developers.facebook.com/apps/');
+    throw new Error(
+        'Provide FACEBOOK_PAGE_ACCESS_TOKEN to .env "Messenger -> Settings -> Access Tokens" https://developers.facebook.com/apps/<YOUR_APP_ID>/messenger/settings/',
+    );
+}
+
+if (FACEBOOK_VERIFY_TOKEN === undefined) {
+    throw new Error(
+        'Provide FACEBOOK_VERIFY_TOKEN to .env. It is a random string, you can invent it yourself. After starting your bot provide it to "Messenger -> Settings -> Webhooks" https://developers.facebook.com/apps/<YOUR_APP_ID>/messenger/settings/',
+    );
 }
 
 const urbanBotFacebook = new UrbanBotFacebook({
