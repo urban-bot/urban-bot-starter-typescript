545989af3957fed7b9359db14f970dcc5d0cf90b
ledamint <heresliker@gmail.com>
2020-06-04 16:17:36 +0300

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,5 +38,8 @@ module.exports = {
                 extensions: ['.js', '.jsx', '.ts', '.tsx'],
             },
         },
+        react: {
+            version: 'detect',
+        },
     },
 };
